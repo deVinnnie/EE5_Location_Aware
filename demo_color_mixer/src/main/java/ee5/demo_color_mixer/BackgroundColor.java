34126fc152fc1ce57cf82be6d48ae7fca96599f8
matthias-mombaerts <matthias.mombaerts@hotmail.be>
2015-05-07 15:47:16 +0200

    Added module demo_color_mixer

---
+++ demo_color_mixer/src/main/java/ee5/demo_color_mixer/BackgroundColor.java
@@ -0,0 +1,8 @@
+package ee5.demo_color_mixer;
+
+/**
+ * Created by Matthias on 24/04/2015.
+ */
+public enum BackgroundColor {
+    YELLOW, RED, BLUE, ORANGE, PURPLE, GREEN
+}
---
+++ demo_color_mixer/src/main/java/ee5/demo_color_mixer/DisplayColorActivity.java
@@ -0,0 +1,53 @@
+package ee5.demo_color_mixer;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class DisplayColorActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_color);
+
+        // Get the colors from the intent
+        Intent intent = getIntent();
+        String myColor = intent.getStringExtra(ColorSelectActivity.EXTRA_MYCOLOR);
+        String otherColor = intent.getStringExtra(ColorSelectActivity.EXTRA_OTHERCOLOR);
+
+        // Show selected colors as text. Later: set background color
+        TextView myTextView = (TextView) findViewById(R.id.myTextView);
+        TextView otherTextView = (TextView) findViewById(R.id.otherTextView);
+        //myTextView.setTextSize(40);
+        myTextView.setText(myColor);
+        //otherTextView.setTextSize(40);
+        otherTextView.setText(otherColor);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_display_color, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
