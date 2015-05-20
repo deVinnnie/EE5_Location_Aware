5ff2e5ed5dd3fcd578f29851c3a5853ac2ca1512
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2015-05-20 16:48:24 +0200

    Set default value to Bluetooth

--- libLocationAware/src/main/java/com/EE5/preferences/SettingsActivity.java
+++ libLocationAware/src/main/java/com/EE5/preferences/SettingsActivity.java
@@ -17,7 +17,7 @@ public class SettingsActivity extends PreferenceActivity {
         /* Set default values for ListViews.*/
         ListPreference connection_type = (ListPreference) findPreference("connection_type");
         if (connection_type.getValue() == null) { // Give a default value
-            connection_type.setValue("TCP");
+            connection_type.setValue("Bluetooth");
         }
         ListPreference socketTaskType = (ListPreference) findPreference("socketTaskType");
         if (socketTaskType.getValue() == null) {
