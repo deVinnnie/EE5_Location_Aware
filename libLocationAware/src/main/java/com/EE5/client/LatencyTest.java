e79cbfcb3895f81e32684d6edd941f80993965ff
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2015-05-21 13:03:14 +0200

    Fix typo

--- libLocationAware/src/main/java/com/EE5/client/LatencyTest.java
+++ libLocationAware/src/main/java/com/EE5/client/LatencyTest.java
@@ -8,7 +8,7 @@ public class LatencyTest {
 
     /**
      *
-     * @return Latency in miliseconds.
+     * @return Latency in milliseconds.
      */
     public static long getLatency(){
         return (System.nanoTime() - LatencyTest.startTime) / 1000000; //1 milisecond = 1000 000 nanoseconds.
