5d59738abae859009c9f6497f2376eb1e7c23714
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2015-05-22 22:18:15 +0200

    Make code more readable
    * Make code more readable
    * Add documentation to and in methods
    * Remove unused code.
    * Limit scope of mIntermediate, ra to method.
    * Move common code outside of if-else statement in method find()
    * Move flipping outside of if-else statement

--- libLocationAware/src/main/java/com/EE5/image_manipulation/PatternDetectorAlgorithm.java
+++ libLocationAware/src/main/java/com/EE5/image_manipulation/PatternDetectorAlgorithm.java
@@ -18,23 +18,19 @@ import java.util.List;
  * The official implementation of the algorithm.
  */
 public class PatternDetectorAlgorithm implements PatternDetectorAlgorithmInterface{
-    private double ra;
-    private int ii;
+    private int ii = 0; //What is ii?
     private int distance;
     private int distance2;
     private boolean setupflag;
 
-    private Scalar orange = new Scalar(255, 120, 0);
+    private Scalar orange = new Scalar(255, 120, 0); //Potential
     private Scalar light_blue = new Scalar(0, 255, 255);
     private Scalar dark_blue = new Scalar(0, 120, 255);
     private Scalar light_green = new Scalar(0, 255, 0);
 
-    private Mat mIntermediateMat = new Mat();
-
     public PatternDetectorAlgorithm(){
         distance2 = 0;
         setupflag = false;
-        ii = 0;
     }
 
     /**
@@ -55,21 +51,16 @@ public class PatternDetectorAlgorithm implements PatternDetectorAlgorithmInterfa
      *         Careful x and y axis are corrected to be compatible with Calc.java convention!!!!!!
      */
     public PatternCoordinator find(Mat rgba, Mat gray2) {
-        double extra_angle = 0;
-
+        //Start timing
         long startTime = System.currentTimeMillis();
-        //Imgproc.resize(gray2,gray2,process_size);
 
-        List<MatOfPoint> contour = new ArrayList<MatOfPoint>();
-        Mat hierarchys = new Mat();
+        List<MatOfPoint> contour = new ArrayList<MatOfPoint>(); //List of all the contours
 
+        Mat mIntermediateMat = new Mat();
         Imgproc.threshold(gray2, mIntermediateMat, 80, 255, Imgproc.THRESH_BINARY); //Apply tresholding, result is stored in 'mIntermediateMat'
-        //Imgproc.threshold(gray2, mIntermediateMat, 80, 255, Imgproc.THRESH_OTSU);
-        //Imgproc.Canny(mIntermediateMat, mIntermediateMat, 80, 90); //mIntermediateMat is a Mat format variable in field
-
 
         //Let OpenCV find contours, the result of this operation is stored in 'contour'.
-        Imgproc.findContours(mIntermediateMat, contour, hierarchys, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+        Imgproc.findContours(mIntermediateMat, contour, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
         List<MatOfPoint> con_in_range;
         List<MatOfPoint> squareContours;
         List<MatOfPoint> pContour;
@@ -77,6 +68,7 @@ public class PatternDetectorAlgorithm implements PatternDetectorAlgorithmInterfa
         MatOfPoint squ_in  = new MatOfPoint();
         MatOfPoint squ_out = new MatOfPoint();
 
+        //Define a default return value for when things should go terribly wrong.
         PatternCoordinator detectedPattern  = new PatternCoordinator(
                 //Pattern in center.
                 new Point(320-50,240-50),
@@ -86,41 +78,46 @@ public class PatternDetectorAlgorithm implements PatternDetectorAlgorithmInterfa
                 0.00
         );
 
+        //Filter out contours with the wrong size.
+        con_in_range = getContoursBySize(distance2, contour);
+        //Filter out non square contours.
+        squareContours = getContoursSquare2(con_in_range);
+        //Find the right contour for the pattern.
+        pContour = findPattern(squareContours);
+
+        //Overlay the image with some useful lines.
+        //Here we draw all potential patterns in orange.
+        Imgproc.drawContours(rgba, squareContours, -1, orange, 4);
+
         if(setupflag == false)
         {
-            //Filter contours with the wrong size.
-            con_in_range = getContoursBySize(distance2, contour);
-            //Filter out non square contours.
-            squareContours = getContoursSquare2(con_in_range);
-            //Find the right contour for the pattern.
-            pContour = findPattern(squareContours);
-            Imgproc.drawContours(rgba, squareContours, -1, orange, 4);
-
-            if (pContour.size() == 2) {
+            if(pContour.size() == 2) {
                 squ_out = pContour.get(1);
                 squ_in = pContour.get(0);
+
                 RotatedRect rot_re_out = new RotatedRect();
                 RotatedRect rot_re_in = new RotatedRect();
+
                 MatOfPoint2f mp2f_out = new MatOfPoint2f();
                 MatOfPoint2f mp2f_in = new MatOfPoint2f();
+
                 mp2f_in = new MatOfPoint2f(squ_in.toArray());
                 mp2f_out = new MatOfPoint2f(squ_out.toArray());
+
                 rot_re_out = Imgproc.minAreaRect(mp2f_out);
                 rot_re_in = Imgproc.minAreaRect(mp2f_in);
 
-                double size_out = rot_re_out.size.area();
-                double size_in = rot_re_in.size.area();
-                ra = size_out/size_in;
-                if((ra>4)&&(ra<16)){
+                double size_out = rot_re_out.size.area(); //Area of the outer black square
+                double size_in = rot_re_in.size.area(); //Area of the inner white square
+
+                double ratio = size_out/size_in;
+                if((ratio>4) && (ratio<16)){ //Check if the ratio between outer and inner square area is correct.
                     ii++;
                     if(ii == 3) {
                         setupflag = true;
                         distance2 = distance2 + 4;
                         ii = 0;
                     }
-                }
-                else{
-                    setupflag = false;
                 }
             }
 
@@ -130,10 +127,6 @@ public class PatternDetectorAlgorithm implements PatternDetectorAlgorithmInterfa
             distance2 = distance2 + 2;
         }
         else {
-            con_in_range = getContoursBySize(distance2, contour);
-            squareContours = getContoursSquare2(con_in_range);
-            pContour = findPattern(squareContours);
-
             Point innerCenter = new Point();
             Point outterCenter = new Point();
 
@@ -157,10 +150,7 @@ public class PatternDetectorAlgorithm implements PatternDetectorAlgorithmInterfa
             List<MatOfPoint> appro_con = new ArrayList<MatOfPoint>();
             appro_con.add(new MatOfPoint(appo.toArray()));
 
-            //Overlay the image with some useful lines.
-            Imgproc.drawContours(rgba, squareContours, -1, orange, 4);
-
-            //Outer
+            //Outer square
             Point a = new Point(NewMtx2.boundingRect().x, NewMtx2.boundingRect().y);
             Point b = new Point(NewMtx2.boundingRect().x + NewMtx2.boundingRect().width, NewMtx2.boundingRect().y + NewMtx2.boundingRect().height);
             Core.rectangle(rgba, a, b, dark_blue, 3);
@@ -181,37 +171,17 @@ public class PatternDetectorAlgorithm implements PatternDetectorAlgorithmInterfa
                 //Core.putText(image, String.valueOf(out_send.getAngle()),new Point(50, 300) , Core.FONT_HERSHEY_SIMPLEX, 1, light_blue);
             }
 
-            //Inner
+            //Inner square
             Point a2 = new Point(NewMtx1.boundingRect().x, NewMtx1.boundingRect().y);
             Point b2 = new Point(NewMtx1.boundingRect().x + NewMtx1.boundingRect().width, NewMtx1.boundingRect().y + NewMtx1.boundingRect().height);
             Core.rectangle(rgba, a2, b2, light_green,3);
 
-            double x1, x2, y1, y2;
-            x1 = innerCenter.x;
-            x2 = outterCenter.x;
-            y1 = innerCenter.y;
-            y2 = outterCenter.y;
+            //Angle
+            double extra_angle = calculateExtraAngle(innerCenter.x, innerCenter.y, outterCenter.x, outterCenter.y);
+            double finalangle = NewMtx2.angle+90+extra_angle;
 
-            double k = (y2 - y1) / (x1 - x2);
-
-            if ((k < -1) | (k > 1)) {
-                if (y2 >= y1) {
-                    extra_angle = 180;
-                } else {
-                    extra_angle = 0;
-                }
-            } else if ((-1 < k) | (k < 1)) {
-                if (x1 >= x2) {
-                    extra_angle = 270;
-                } else {
-                    extra_angle = 90;
-                }
-            }
-
-            double finalangle =NewMtx2.angle+90+extra_angle;
-
-            String kk = "k is (" + String.valueOf(k) + ")";
-            Core.putText(rgba, kk, new Point(50, 400), Core.FONT_HERSHEY_SIMPLEX, 1, light_blue);
+            //String kk = "k is (" + String.valueOf(k) + ")";
+            //Core.putText(rgba, kk, new Point(50, 400), Core.FONT_HERSHEY_SIMPLEX, 1, light_blue);
             String dis = "the distance2 is "+ String.valueOf(distance2) +" )";
             Core.putText(rgba, dis, new Point(50, 350), Core.FONT_HERSHEY_SIMPLEX, 1, light_blue);
             String angle = "rotate angle is (" + String.valueOf(finalangle) + ")";
@@ -225,34 +195,70 @@ public class PatternDetectorAlgorithm implements PatternDetectorAlgorithmInterfa
             if(out_send.getNum2() !=  null) {
                 pc = new PatternCoordinator(out_send.getNum1(), out_send.getNum2(), out_send.getNum3(), out_send.getNum4(), finalangle);
             }
-
-            detectedPattern = new PatternCoordinator(
-                    new Point(pc.getNum1().y, pc.getNum1().x),
-                    new Point(pc.getNum2().y, pc.getNum2().x),
-                    new Point(pc.getNum3().y, pc.getNum3().x),
-                    new Point(pc.getNum4().y, pc.getNum4().x),
-                    0.0
-            );
+            detectedPattern = pc;
         }
+
+        detectedPattern = new PatternCoordinator(
+            //Flip the coordinates so they are in the right convention for the Calc class.
+            new Point(detectedPattern.getNum1().y, detectedPattern.getNum1().x),
+            new Point(detectedPattern.getNum2().y, detectedPattern.getNum2().x),
+            new Point(detectedPattern.getNum3().y, detectedPattern.getNum3().x),
+            new Point(detectedPattern.getNum4().y, detectedPattern.getNum4().x),
+            0.0
+        );
+
         return detectedPattern;
     }
 
+    private double calculateExtraAngle(double x1, double y1, double x2, double y2){
+        //Calculate the angle.
+        double k = (y2 - y1) / (x1 - x2);
+        double extra_angle=0;
+
+        if ((k < -1) | (k > 1)) {
+            if (y2 >= y1) {
+                extra_angle = 180;
+            } else {
+                extra_angle = 0;
+            }
+        } else if ((-1 < k) | (k < 1)) {
+            if (x1 >= x2) {
+                extra_angle = 270;
+            } else {
+                extra_angle = 90;
+            }
+        }
+        return extra_angle;
+    }
+
+    /**
+     * Put the points in the right order so that they conform to the convention.
+     * This method will rotate the given list of points until it finds the order where the first point is closest to the inner white square.
+     *
+     * @param point List of points representing the points of the pattern.
+     * @param in_center Center of the small inner white square.
+     * @return The points of the pattern in the right order.
+     *          The angle is this patternCoordinator object is to be interpreted
+     *          as the distance between Point 1 and the white square.
+     */
     private PatternCoordinator Cal_Pointnum(Point[] point, Point in_center){
         Point[] point_send = new Point[4];
-        double dis=0;
-        double min_dis=10000000;
+        double distance = 0; //Distance from Point to Inner Center.
+        double min_dis= Double.POSITIVE_INFINITY; //Initialize at infinity.
 
         for (int i = 0;i<4;i++){
-            dis = Math.sqrt(Math.pow(in_center.x - point[i].x,2)+Math.pow(in_center.y-point[i].y,2));
-            if(dis<min_dis){
-                min_dis = dis;
+            distance = Math.sqrt(Math.pow(in_center.x - point[i].x,2)+Math.pow(in_center.y-point[i].y,2));
+            if(distance<min_dis){
+                //If the current point is closer to the white square then rearrange the points
+                // so that the current point is the first point.
+                min_dis = distance;
                 //TODO Remove IF-statements and replace with something 'clever'
                 if(i == 0){
                     point_send[0] = point[0];
                     point_send[1] = point[1];
                     point_send[2] = point[2];
                     point_send[3] = point[3];
-                };
+                }
                 if(i == 1) {
                     point_send[0] = point[1];
                     point_send[1] = point[2];
@@ -273,27 +279,39 @@ public class PatternDetectorAlgorithm implements PatternDetectorAlgorithmInterfa
                 }
             }
         }
-        PatternCoordinator pc = new PatternCoordinator(point_send[0],point_send[1],point_send[2],point_send[3],dis);
+        PatternCoordinator pc = new PatternCoordinator(point_send[0],point_send[1],point_send[2],point_send[3],distance);
         return pc;
     }
 
+    /**
+     * Filter out the contours in contour whose contourArea is smaller than dis*600 or larger than dis*10.
+     *
+     * @param dis
+     * @param contour List of contours
+     * @return
+     */
     private List<MatOfPoint> getContoursBySize(int dis, List<MatOfPoint> contour) {
         List<MatOfPoint> con_in_range = new ArrayList<MatOfPoint>();
-        //filter out contours out of certain range
         Iterator<MatOfPoint> each = contour.iterator();
         while (each.hasNext()) {
             MatOfPoint contours = each.next();
-            if ((Imgproc.contourArea(contours) < dis * 600) & (Imgproc.contourArea(contours) > dis * 10)) {
+            if ((Imgproc.contourArea(contours) < (dis * 600)) & (Imgproc.contourArea(contours) > (dis * 10))) {
                 con_in_range.add(contours);
             }
         }
         return con_in_range;
     }
 
+    /**
+     * Find contours which bestly resemble a square shape (sides of the bounding box are equal)
+     *
+     * @param con_in_range
+     * @return
+     */
     private List<MatOfPoint> getContoursSquare2(List<MatOfPoint> con_in_range) {
         List<MatOfPoint> squareContours = new ArrayList<MatOfPoint>();
-        List<Point> cl = new ArrayList<Point>();
-        Iterator<Point> con;
+        //List<Point> cl = new ArrayList<Point>();
+        //Iterator<Point> con;
         Iterator<MatOfPoint> each_con = con_in_range.iterator();
         Rect out_rect = new Rect();
 
@@ -429,6 +447,11 @@ public class PatternDetectorAlgorithm implements PatternDetectorAlgorithmInterfa
     }*/
     //</editor-fold>
 
+    /**
+     *
+     * @param shapeContour
+     * @return The center point of the shape.
+     */
     private Point getShapeCenter(MatOfPoint shapeContour){
         Iterator<Point> con_point = shapeContour.toList().iterator();
         Point shapeCenter = new Point();
@@ -436,18 +459,19 @@ public class PatternDetectorAlgorithm implements PatternDetectorAlgorithmInterfa
         double y_sum = 0;
         int count = 0;
         while(con_point.hasNext()){
+            //Add all x and y values.
             Point pt = con_point.next();
             x_sum += pt.x;
             y_sum += pt.y;
             count++;
         }
-        shapeCenter.x = x_sum/count;
+        shapeCenter.x = x_sum/count; //Take the average.
         shapeCenter.y = y_sum/count;
         return shapeCenter;
     }
 
     private List<MatOfPoint> findPattern(List<MatOfPoint> contours){
-        double distance = 1000000;
+        double distance = Double.POSITIVE_INFINITY;
         List<MatOfPoint> patternContours = new ArrayList<MatOfPoint>();
         Iterator<MatOfPoint> iter1 = contours.iterator();
         while(iter1.hasNext()){
@@ -457,8 +481,8 @@ public class PatternDetectorAlgorithm implements PatternDetectorAlgorithmInterfa
             while(iter2.hasNext()){
                 MatOfPoint con2 = iter2.next();
                 Point center2 = getShapeCenter(con2);
-                double dis_this = Math.abs(center1.x-center2.x)+Math.abs(center1.y-center2.y);
-                if((dis_this < distance)&(dis_this>0)){//&(dis_this<70)){
+                double dis_this = Math.abs(center1.x-center2.x)+Math.abs(center1.y-center2.y); //Distance between the the centers of inner and outer square.
+                if((dis_this < distance) & (dis_this>0)){//&(dis_this<70)){
                     distance = dis_this;
                     patternContours.clear();
                     patternContours.add(con1);
