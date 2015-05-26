bca8dd22d6c2445f3d5e6e66104994df566155b2
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2015-05-26 14:12:22 +0200

    Remove silly code

--- libLocationAware/src/main/java/com/EE5/SetupActivity.java
+++ libLocationAware/src/main/java/com/EE5/SetupActivity.java
@@ -61,25 +61,7 @@ public class SetupActivity extends Activity {
 
         return true;
     }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            startActivity(item.getIntent());
-            return true;
-        }
-
-        if (id == R.id.action_calibrate){
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
+    
     @Override
     protected void onPause() {
         super.onPause();
