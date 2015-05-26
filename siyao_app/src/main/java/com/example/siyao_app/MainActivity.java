7af029ecb6a390443a3238adba51b19e6573bb80
Vincent Ceulemans <deVinnnie@users.noreply.github.com>
2015-05-26 22:00:34 +0200

    Finish demo app

--- siyao_app/src/main/java/com/example/siyao_app/MainActivity.java
+++ siyao_app/src/main/java/com/example/siyao_app/MainActivity.java
@@ -1,75 +1,84 @@
 package com.example.siyao_app;
-import com.EE5.math.Calc;
-import com.EE5.image_manipulation.PatternCoordinator;
-import com.EE5.server.data.Position;
-import com.EE5.util.GlobalResources;
-import com.EE5.util.Point3D;
-import com.EE5.util.Tuple;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-import com.example.slaver;
+
+import com.EE5.math.Calc;
+import com.EE5.server.data.Position;
+import com.EE5.util.GlobalResources;
+import com.EE5.util.Tuple;
+
 import java.util.Map;
 
-public class MainActivity extends ActionBarActivity {
+
+public class MainActivity extends Activity {
     private EditText et1;
-    private Button bt1;
-    private int number1,number2,number3,number4;
+    private Button bt1,bt2;
+    private int number1,number3,number4;
+    private int parsedNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        startActivityForResult(new Intent("com.EE5.image_manipulation.ImageManipulationsActivity"), 0);
+        startActivityForResult(new Intent(this, com.EE5.SetupActivity.class), 0);
         et1 = (EditText)findViewById(R.id.number1);
-        bt1 = (Button)findViewById(R.id.button_start);
-      //  bt2 = (Button)findViewById(R.id.button_restart);
-        bt1.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View arg0) {
 
+        bt1 = (Button)findViewById(R.id.button_start);
+
+        bt2 = (Button)findViewById(R.id.button_send);
+
+        bt1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
                 String str1 = et1.getText().toString();
                 number1 = Integer.parseInt(str1);
-                number2 = number_slave;
-                number3 = number2 + number1;
-                number4 = number1 * number2;
-                //System.out.println(number3);
 
-                Position ownPosition = GlobalResources.getInstance().getDevice().getPosition();
-                Position otherPosition = new Position(0,0,0,0);
-
-               // GlobalResources.getInstance().setData();
-
-                for (Map.Entry<String, Tuple<Position, String>> entry : GlobalResources.getInstance().getDevices().getAll()){
+                String number = "1";
+                Position otherPosition = new Position();
+                for (Map.Entry<String, Tuple<Position, String>> entry : GlobalResources.getInstance().getDevices().getAll()) {
+                    number = entry.getValue().element2;
                     otherPosition = entry.getValue().element1;
                     break; //Only read the position of the first device.
                 }
+                parsedNumber = Integer.parseInt(number);
 
-               // double distance = Calc.getDistance(ownPosition,otherPosition);
-               // int distance=15;
-               if(distance < 20) {
-                   display(number3);
-               }
-                else{
-                   display(number4);
-               }
-                //Dostop(number4);
+                Position ownPosition = GlobalResources.getInstance().getDevice().getPosition();
+
+                number3 = number1 + parsedNumber;
+                number4 = number1 * parsedNumber;
+
+                // double distance = Calc.getDistance(ownPosition,otherPosition);
+                // int distance=15;
+                double distance = Calc.getDistance(ownPosition, otherPosition);
+                if (distance < 20) {
+                    display(number3);
+                } else {
+                    display(number4);
+                }
             }
+
+
         });
 
-
+        bt2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View arg0) {
+                //Send Number
+                String str1 = et1.getText().toString();
+                number1 = Integer.parseInt(str1);
+                display(number1);
+                GlobalResources.getInstance().setData(et1.getText().toString());
+            }
+        });
     }
     private void display(int number){
-
         Toast.makeText(this,""+number, 20).show();
-
     }
     @Override
     public boolean onCreateOptionsMenu ( Menu menu) {
